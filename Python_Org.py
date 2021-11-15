941574e75fb21229cbb6a8643c681c771dace5e4
boborodono <bowen.wilder@GMAIL.COM>
2021-11-14 20:33:50 -0600

    Cleaned code for submission

---
+++ Python_Org.py
@@ -0,0 +1,6 @@
+# Dependecies:
+
+# Add a variable to load a file from a path.
+#file_to_load = os.path.join("Resources", "election_results.csv")
+# Add a variable to save the file to a path.
+#file_to_save = os.path.join("analysis", "election_analysis.txt")
